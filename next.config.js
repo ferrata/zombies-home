c6ecf433b60bc8f7b2bd079e502e531f6dafddd2
Sasha Goloshchapov <31716295+ferrata@users.noreply.github.com>
2024-02-27 02:58:56 -0600

    feat: sounds (#1)

--- next.config.js
+++ next.config.js
@@ -3,6 +3,22 @@ const withMakeswift = require('@makeswift/runtime/next/plugin')()
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  webpack: config => {
+    config.module.rules.push({
+      test: /\.ogg$/,
+      use: {
+        loader: 'file-loader',
+        options: {
+          publicPath: '/_next/static/sounds/',
+          outputPath: 'static/sounds/',
+          name: '[path][name].[ext]',
+        },
+      },
+    })
+
+    return config
+  },
 }
 
 module.exports = withMakeswift(nextConfig)
